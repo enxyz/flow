b73ed3e996a273dd67e2d5c1a901994784f9fbde
Eric Norman <eric@norman.zone>
2021-01-15 22:21:52 -0800

    Runs up to setting local env auth to none.

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React, { Suspense } from 'react';
+import { render, waitForElement } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+test('renders learn react link', async () => {
+  const { getByText, queryByTestId } = render(
+    <RecoilRoot>
+      <Suspense fallback={<div>test loading...</div>}>
+        <MemoryRouter>
+          <App data-testid="test-app" />
+        </MemoryRouter>
+      </Suspense>
+    </RecoilRoot>
+  );
+  await waitForElement(() => queryByTestId('test-app'));
   const linkElement = getByText(/Flow by LabGrid/i);
   expect(linkElement).toBeInTheDocument();
 });
